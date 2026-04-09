49a16680756043355e1b7a71e58f520621e16fb5
deepakstack-46 <deepak.kudiyal@itas.ca>
2026-04-09 14:26:58 -0700

    updtaded model error

--- accounts/models.py
+++ accounts/models.py
@@ -2,5 +2,5 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
-class User(AttributeError):
+class User(AbstractUser):
     phone = models.CharField(max_length=10)
