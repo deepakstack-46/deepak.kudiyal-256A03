bdd74f6f4eb7bf7c96b08847d46b5bdd688729b6
deepakstack-46 <deepak.kudiyal@itas.ca>
2026-04-10 10:36:25 -0700

    added path and positions onf installed apps

--- happy_camp/settings.py
+++ happy_camp/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'accounts',
     # 'events',
-    # 'positions',
+    'positions',
     # 'report',
     
 ]
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'happy_camp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -90,16 +90,11 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'OPTIONS': {'min_length': 8}
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        'NAME': 'accounts.validators.CustomPasswordValidator',
     },
 ]
 
