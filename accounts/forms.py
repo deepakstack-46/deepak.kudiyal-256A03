bf7b74807081cc5d3f043eae383819d54c9ef11a
deepakstack-46 <deepak.kudiyal@itas.ca>
2026-04-09 22:56:03 -0700

    made the form section

---
+++ accounts/forms.py
@@ -0,0 +1,48 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from .models import User    
+import re
+
+class RegistrationForm(UserCreationForm):
+    first_name = forms.CharField(max_length=50)
+    last_name = forms.CharField(max_length=50)
+    email = forms.EmailField()
+    phone = forms.CharField(max_length=10) 
+    ROLE_CHOICES = (
+        ('Member', 'member'),
+        ('Volunteer', 'volunteer'),
+    )
+    role = forms.ChoiceField(choices=ROLE_CHOICES)
+
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'email', 'phone', 'password1', 'password2']
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields.pop('username', None)
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if User.objects.filter(email=email).exists():
+            raise forms.ValidationError("Email already exists")
+        return email
+
+    def clean_phone(self):
+        phone = self.cleaned_data.get('phone')
+        if not re.match(r'^\d{10}$', phone):
+            raise forms.ValidationError("Phone number must be 10 digits")
+        return phone    
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.username = self.cleaned_data['email']
+        user.email = self.cleaned_data['email']
+        if commit:
+            user.save()
+        return user
+
+class LoginForm(AuthenticationForm):
+    username = forms.CharField(label='Email')
+    password = forms.CharField(widget=forms.PasswordInput)
+
