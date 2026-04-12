6586926f597f6750c257a46b70ada713675905c7
deepakstack-46 <deepak.kudiyal@itas.ca>
2026-04-11 22:17:28 -0700

    completed events

--- events/views.py
+++ events/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.utils import timezone
-from . models import Event, EventRegistration
+from .models import Event, EventRegistration
 from positions.models import Position
 
 # Create your views here.
