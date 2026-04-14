6ea891dd8c210397d532ff0a9a2f2d4ec1497c93
deepakstack-46 <deepak.kudiyal@itas.ca>
2026-04-14 10:04:30 -0700

    fixed error

--- accounts/validators.py
+++ accounts/validators.py
@@ -11,8 +11,6 @@ class CustomPasswordValidator:
             raise ValidationError("Password must contain at least one lowercase letter.")
         if not re.search(r'\d', password):
             raise ValidationError("Password must contain at least one digit.")
-        if not re.search(r'[!@#$%^&*(),.?":{}|<>]', password):
-            raise ValidationError("Password must contain at least one special character.")
-
+       
     def get_help_text(self):
         return "Your password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, one digit, and one special character."
