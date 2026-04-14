1720d30828a30bb366783aefedd7f7a47601bd2c
deepakstack-46 <deepak.kudiyal@itas.ca>
2026-04-14 10:02:11 -0700

    fixed error

--- positions/views.py
+++ positions/views.py
@@ -20,9 +20,9 @@ def position_create(request):
         return redirect('home')
 
     if request.method == 'POST':
-        print(request.POST)
+        
         position_name = request.POST.get('position_name')
-        print(position_name)
+        
         if position_name:
             Position.objects.create(position_name=position_name)
             
